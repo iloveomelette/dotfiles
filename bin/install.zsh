c22be88ef3c30f748d4d4a50f7f54bf3809b42ff
iloveomelette <mizuki1998hosei@gmail.com>
2025-03-14 00:54:54 +0900

    :adhesive_bandage: remove unnecessary code and modify steps

--- bin/install.zsh
+++ bin/install.zsh
@@ -12,16 +12,6 @@ else
 fi
 
 #
-# ==================================
-# Fetch the newest version of zinit
-# ==================================
-#
-
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-  zsh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-fi
-
-#
 # =====
 # Setup
 # =====
